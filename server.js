fec389fa68391847198203b5c665e70d2aa5a71f
estherRGU <e.w.koloh@rgu.ac.uk>
2016-08-17 04:57:37 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,49 @@
+"use strict"                                    // Ensures JavaScript coding rules are strictly followed
+
+// setup application
+
+// set up ======================================================================
+// get all the tools we need by calling all required packages
+var express  = require('express');              // 
+var app      = express();                       //
+var port     = process.env.PORT || 8080;        //
+var mongoose = require('mongoose');             //
+var mongodb = require('mongodb');
+var passport = require('passport');             //
+var flash    = require('connect-flash');        //
+
+var morgan       = require('morgan');           //
+var cookieParser = require('cookie-parser');    //
+var bodyParser   = require('body-parser');      //
+var session      = require('express-session');  //
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+module.exports = {
+     'url' : 'mongodb://estherk:255132@jello.modulusmongo.net:27017/iHigi6go'
+     };
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovethismscproject' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./models/routes.js')(app, passport); // load the routes and pass in the app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic of  data visualisation happens on port ' + port);
